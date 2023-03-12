fac90b2622deabb01e8f6043394a5aab6cf5c6dc
JairontSantos <jairon.tavares@hotmail.com>
2023-03-12 20:41:05 -0300

    Update #Projeto Biblioteca.py

--- #Projeto Biblioteca.py
+++ #Projeto Biblioteca.py
@@ -2,7 +2,6 @@
 
 class Livro:
 
-
     def __init__(self, prev=None, next=None, name=None, author=None):
         self.prev = prev
         self.next = next
@@ -14,36 +13,29 @@ class Estante:
         self.head = None
         self.tail = None
 
-    def append(self, new_livro: Livro):
-        
+    def append(self, new_livro:Livro):
+
         if self.head is None:
             print('=' * 55)
-            print(f'Você está colocando o primeiro livro na prateleira! Obrigado!')
-            print('=' * 55)
-        else: 
-            print(f'Você está colocando o livro: {self.head.name}, do autor: {self.head.author}, no final da prateleira! Obrigado! ')
-            print('=' * 55)
-        
+            print(f'Você está colocando o primeiro livro na prateleira! Obrigado! ')
+
         if self.head is None:
             self.head = new_livro
             self.tail = new_livro
             return
-        
+
         new_livro.prev = self.tail
         self.tail.next = new_livro
         self.tail = new_livro
 
-    def prepend(self, new_livro: Livro):
+        print('=' * 55)
+        print(f'Você está colocando o livro {self.tail.name} do autor {self.tail.author} no final da prateleira! Obrigado! ')
+
+    def prepend(self, new_livro:Livro):
 
         if self.head is None:
             print('=' * 55)
-            print(f'Você está colocando o primeiro livro na prateleira! Obrigado!')
-            print('=' * 55)
-        else: 
-            print(f'Você está colocando o livro: {self.head.name}, do autor: {self.head.author}, no inicio da prateleira! Obrigado! ')
-            print('=' * 55)
-
-        if self.head is None:
+            print(f'Você está colocando o primeiro livro na prateleira! Obrigado! ')
             self.head = new_livro
             self.tail = new_livro
             return
@@ -52,61 +44,72 @@ class Estante:
         self.head.prev = new_livro
         self.head = new_livro
 
-    def remove_end(self):
-        if self.head is None:  # Lista vazia
+        print('=' * 55)
+        print(f'Você está colocando o livro {self.head.name} do autor {self.head.author} no inicio da prateleira! Obrigado! ')
+
+    def remove_last(self):
+
+        if self.head is None:
+            print(f'=' * 55)
+            print('Não possui nenhum livro na estante para tirar!')
             return
         
-        if self.head.prev and self.head.next is None:  # A lista tem apenas um nó
+        if self.head.next is None: 
             self.head = None
             self.tail = None
             return
 
-        print(f'Você está tirando o livro: {self.head.name}, do final da estante!')
         print('=' * 55)
+        print(f'Você está tirando o livro: {self.tail.name}, do autor {self.tail.author} do final da estante!')
 
         self.tail = self.tail.prev
         self.tail.next = None
 
-
-
-    def remove_beginning(self):
+    def remove_first(self):
         if self.head is None:
-            return 0
+            print('Não possui nenhum livro na estante para tirar!')
+            print(f'=' * 55)
+            return 
         
         if self.head.prev and self.head.next is None:
             self.head = None
             self.tail = None
             return 
         
-        print(f'Você está tirando o livro: {self.head.name}, do ínicio da estante!')
+        print('=' * 55)
+        print(f'Você está tirando o livro: {self.head.name}, do autor {self.head.author} do ínicio da estante!')
         print('='* 55)
 
-        NotImplemented
-
+        self.head = self.head.next
+        self.head.prev = None
 
     def revision(self):
-        contagem = 0
         current = self.head
+        contagem = 0
         while current is not None:
             print(f'O nome do livro é: {current.name}')
             print(f'E seu autor é : {current.author}')
-            current = current.next
             print('=' * 55)
+            current = current.next
             contagem += 1
-        print(f'Estamos no momento com {contagem} livros na estante')
+        print(f'A quantidade de livros na estante é {contagem}!')
         print('=' * 55)
 
-estante = Estante()
+list = Estante()
 
 li = Livro(name='Alice', author='JK Rowling')
-li2 = Livro(name='Academia do Grego', author='Platão')
-li3 = Livro(name='Diário de um Banana', author='N/A')
-li4 = Livro(name='Manga JoJo', author='Araki')
+li1 = Livro(name='Minecraft', author='N/A')
+li2 = Livro(name='JoJo', author='Araki')
+li3 = Livro(name='1984', author='Não sei')
 
-estante.prepend(li)
-estante.prepend(li2)
-estante.append(li3)
+list.prepend(li)
+list.prepend(li2)
+list.prepend(li1)
+list.append(li3)
 
-estante.remove_end()
+list.remove_last()
+list.remove_last()
+list.remove_first()
 
-estante.revision()
+
+list.revision()
